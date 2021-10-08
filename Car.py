9f03091999289323ddee9e0987763274a4211051
Buhary Mohammed Rasith <77905051+rasith1998@users.noreply.github.com>
2021-10-08 19:12:55 +0530

    Add files via upload

---
+++ Car.py
@@ -0,0 +1,4 @@
+class Car:
+    brand=""
+    def __init__(self, brand):
+        self.brand = brand
---
+++ program.py
@@ -0,0 +1,6 @@
+from Car import Car
+car1 = Car("A")
+car2 = Car("B")
+print("Hello")
+print(car1.value)
+print(car2.value)
